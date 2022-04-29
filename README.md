d8b8630fbef30bef78224564f1a3d11f1f5aedaa
TAMASHCHUK YAN <101806907+Yanobivan@users.noreply.github.com>
2022-04-29 21:42:58 +0300

    Yan-webpack-project

---
+++ README.md
@@ -0,0 +1,11 @@
+Basic-tsx-webpack-build
+
+huski;
+tsx;
+babel, babel-plugin-transform-imports;
+scss;
+postCSS;
+eslint;
+prettier;
+
+"config" from 'https://github.com/taniarascia/webpack-boilerplate'
---
+++ config/webpack.common.js
@@ -0,0 +1,83 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const paths = require('./paths')
+
+module.exports = {
+  entry: [paths.src + '/index.tsx'],
+
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.public,
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+
+    new HtmlWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      { test: /\.js$/, use: ['babel-loader'] },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            plugins: [
+              [
+                require('babel-plugin-transform-imports'),
+                {
+                  'my-library': {
+                    transform: function (importName) {
+                      return 'my-library/etc/' + importName.toUpperCase()
+                    },
+                    preventFullImport: true,
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+      { test: /\.xml$/, use: ['xml-loader'] },
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+    ],
+  },
+
+  resolve: {
+    modules: [paths.src, 'node_modules'],
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    alias: {
+      '@': paths.src,
+      // assets: paths.public,
+    },
+  },
+}
+
+module: {
+  rules: [
+    {
+      test: /\.js$/,
+    },
+  ]
+}
